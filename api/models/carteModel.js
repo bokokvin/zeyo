5b2265ecce1d43c0aff75797b3cf54dfea9e9277
DESKTOP-J9R3STS\MALID <bokokvin@gmail.com>
2019-09-29 10:40:07 +0200

    Update carteModel.js

--- api/models/carteModel.js
+++ api/models/carteModel.js
@@ -5,9 +5,12 @@ var Schema = mongoose.Schema;
 
 var carteSchema = new Schema({
     date_emission   : { type: Date, default: Date.now},
-    code            : Number,
-    value           : Number,
-    status          : String
+    code            : { type: Number, 
+                        default : function() { return Math.floor(Math.random()*900000000300000000000) + 1000000000000000},
+                        index: { unique: true }
+                    },
+    value           : { type: Number, default: 1000},
+    status          : { type: Boolean, default: 0},
     //carteValue:  { type: Schema.Types.ObjectId, ref: 'CarteValue', required: true}
 });
 
